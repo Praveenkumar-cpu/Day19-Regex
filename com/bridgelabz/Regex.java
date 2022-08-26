1f3ab6cdd702f83c0ffc2db3a52005e6a013b353
praveen <praveenpkbadiger@gmail.com>
2022-08-26 14:40:42 +0530

    Added: Lname-Validation

--- com/bridgelabz/Regex.java
+++ com/bridgelabz/Regex.java
@@ -9,7 +9,7 @@ public class Regex {
     static void validateFirstName() {
         // first name cap and min 3 char
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Fname");
+        System.out.println("Enter FirstName");
         String fname=sc.next();
         String regex = "[A-Z]{1}+[a-z]{3,}+$";
         Pattern p = Pattern.compile(regex);
@@ -20,13 +20,34 @@ public class Regex {
             System.out.println("Valid Name");
         }
         else {
-            System.out.println("Invalid Name");
+            System.out.println("Please Enter valid Name");
+        }
+    }
+
+    
+    static void validateLName() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter LastName");
+        String lname=sc.next();
+        String regex = "[A-Z]{1}+[a-z]{3,}+$";
+        Pattern p = Pattern.compile(regex);
+        Matcher matcher = p.matcher(lname);
+        boolean result = matcher.matches();
+
+        if(result){
+            System.out.println("Valid Name");
+        }
+        else {
+            System.out.println("Please Enter valid Name");
         }
     }
 
 
+
     public static void main(String[] args) {
         validateFirstName();
+        validateLName();
     }
 
 }
